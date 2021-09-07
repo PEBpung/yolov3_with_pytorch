e539c99c76611f65376fe18a26d4e0750d201155
PEBpung <mcuwwa29@gmail.com>
2021-09-07 09:10:21 +0900

    [Add] Step3. ResidualBlock

--- model.py
+++ model.py
@@ -47,7 +47,24 @@ class CNNBlock(nn.Module):
             return self.conv(x)
 
 class ResidualBlock(nn.Module):
-    pass
+    def __init__(self, channels, use_residual=True, num_repeats=1):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for _ in range(num_repeats):
+            self.layers += {
+                nn.Sequential(
+                    CNNBlock(channels, channels//2, kernel_size=1),
+                    CNNBlock(channels//2, channels, kernel_size=3, padding=1)
+                )
+            }
+        self.use_residual = use_residual
+        self.num_repeats = num_repeats
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x) + x if self.use_residual else layer(x)
+        return x
+
 
 class ScalePrediction(nn.Module):
     pass
