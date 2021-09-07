cd97522cb89da85d7040b87adfa196b26ad06b00
PEBpung <mcuwwa29@gmail.com>
2021-09-07 09:03:57 +0900

    [Add] Step2. CNNBlock

--- model.py
+++ model.py
@@ -31,3 +31,26 @@ config = [
     (256, 3, 1),
     "S",
 ]
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, bn_act = True, **kwargs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=not bn_act, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.leaky = nn.LeakyReLU(0.1)
+        self.use_bn_act = bn_act
+
+    def forward(self, x):
+        if self.use_bn_act:
+            return self.leaky(self.bn(self.conv(x)))
+        else:
+            return self.conv(x)
+
+class ResidualBlock(nn.Module):
+    pass
+
+class ScalePrediction(nn.Module):
+    pass
+
+class YOLOv3(nn.Module):
+    pass
