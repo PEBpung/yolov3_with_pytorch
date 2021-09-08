de8111ee6983465610a5c918e245e8b8ecfd5437
PEBpung <mcuwwa29@gmail.com>
2021-09-08 09:11:49 +0900

    [Add] Step5. Yolov3

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-# Tuple : (out_channels, kernel_size, stride)
-# List : ["B", num_repeats] 
-# 'S' : branch, 중간 사진
+# Tuple : (out_channels, kernel_size, stride), CNN 모델
+# List : ["B", num_repeats] , 반복 횟수
+# 'S' : branch, 중간 사진, Sacle prediction
 # 'U' : Upsampling 
 config = [
     (32, 3, 1),
@@ -70,17 +70,94 @@ class ScalePrediction(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
         self.pred = nn.Sequential(
-            CNNBlock(in_channels, 2*in_channels, kernel_size=3, padding=1),
-            CNNBlock(2*in_channels, 3 * (num_classes + 5) * 3, bn_act=False, kernel_size=1), #[po, x, y, w, h]
+            CNNBlock(in_channels, 2 * in_channels, kernel_size=3, padding=1),
+            CNNBlock(
+                2 * in_channels, (num_classes + 5) * 3, bn_act=False, kernel_size=1
+            ),
         )
         self.num_classes = num_classes
 
     def forward(self, x):
-        return(
+        return (
             self.pred(x)
             .reshape(x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3])
-            .permute(0, 1, 3, 4, 2) # dimension의 순서를 바꿔준다.
+            .permute(0, 1, 3, 4, 2)
         )
         # ex> N x 3 x 13 x 13 x 5+num_classes
+
 class YOLOv3(nn.Module):
-    pass
+    def __init__(self, in_channels=3, num_classes=80):
+        super().__init__()
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.layers = self._create_conv_layers()
+
+    def forward(self, x):
+        outputs = []  # for each scale
+        route_connections = []
+        for layer in self.layers:
+            if isinstance(layer, ScalePrediction):
+                outputs.append(layer(x))
+                continue
+
+            x = layer(x)
+
+            if isinstance(layer, ResidualBlock) and layer.num_repeats == 8:
+                route_connections.append(x)
+
+            elif isinstance(layer, nn.Upsample):
+                x = torch.cat([x, route_connections[-1]], dim=1)
+                route_connections.pop()
+
+        return outputs
+                
+    def _create_conv_layers(self):
+        layers = nn.ModuleList()
+        in_channels = self.in_channels
+
+        for module in config:
+            if isinstance(module, tuple):
+                out_channels, kernel_size, stride = module
+                layers.append(
+                    CNNBlock(
+                        in_channels,
+                        out_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        padding=1 if kernel_size == 3 else 0,
+                    )
+                )
+                in_channels = out_channels
+
+            elif isinstance(module, list):
+                num_repeats = module[1]
+                layers.append(ResidualBlock(in_channels, num_repeats=num_repeats,))
+
+            elif isinstance(module, str):
+                if module == "S":
+                    layers += [
+                        ResidualBlock(in_channels, use_residual=False, num_repeats=1),
+                        CNNBlock(in_channels, in_channels // 2, kernel_size=1),
+                        ScalePrediction(in_channels // 2, num_classes=self.num_classes),
+                    ]
+                    in_channels = in_channels // 2
+
+                elif module == "U":
+                    layers.append(nn.Upsample(scale_factor=2),)
+                    in_channels = in_channels * 3
+                    # 왜 in_channel에 3을 곱해주는 걸까?
+                    # Upsampling된 레이어를 바로 concat하지 못해서 그런가?
+                
+        return layers
+
+
+if __name__ == "__main__":
+    num_classes = 20
+    IMAGE_SIZE = 416
+    model = YOLOv3(num_classes=num_classes)
+    x = torch.randn((2, 3, IMAGE_SIZE, IMAGE_SIZE))
+    out = model(x)
+    assert model(x)[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, num_classes + 5)
+    assert model(x)[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, num_classes + 5)
+    assert model(x)[2].shape == (2, 3, IMAGE_SIZE//8, IMAGE_SIZE//8, num_classes + 5)
+    print("Success!")
